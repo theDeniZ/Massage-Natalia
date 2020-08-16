2282cc844e940a15c5f43e3f939a05f9b5b85374
Denis Dobanda <theDeniZ@icloud.com>
2020-08-16 18:38:34 +0200

    B| Naming fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mass'));
+app.use(express.static(__dirname + '/dist/Mass'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/mass/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Mass/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
