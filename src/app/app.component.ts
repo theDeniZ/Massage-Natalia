d58b18c7889522528248cf71dade80e0de760728
Denis Dobanda <theDeniZ@icloud.com>
2020-08-16 18:33:56 +0200

    F| Text and basic layout update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mass';
+  title = 'Massage Natalia';
+  @Input() feedbackThanks = false;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/mnqglawl',
+          { name: email.Name, replyto: email.Email, message: email.Message },
+          { 'headers': headers }).subscribe(
+          response => {
+            console.log(response);
+            contactForm.setValue({ Name: '', Email: '', Message: '' });
+            this.feedbackThanks = true;
+          }
+      );
+    }
+  }
 }
